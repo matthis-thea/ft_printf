94f581d342cf7c2120ece19e39ebff2115dbf280
lamalicce <matthea834@gmail.com>
2022-11-26 09:41:38 +0100

    finit

--- SRC/ft_printf.c
+++ SRC/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: haze <haze@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 13:02:06 by mthea             #+#    #+#             */
-/*   Updated: 2022/11/25 15:31:44 by haze             ###   ########.fr       */
+/*   Updated: 2022/11/26 09:40:43 by haze             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 int	ft_print_value(va_list parametres, char str, int *verification)
 {
-	int		value;
+	int	value;
 
-	value = 0;
 	if (str == 'c')
 		value = ft_printf_c(va_arg(parametres, int), verification);
 	else if (str == 's')
 		value = ft_printf_s(va_arg(parametres, char *), verification);
 	else if (str == 'p')
-		value = ft_printf_void(va_arg(parametres, unsigned long long), "0123456789ABCDEF",verification);
+		value = ft_printf_void(va_arg(parametres, unsigned long long), "0123456789abcdef",verification);
 	else if ((str == 'd') || (str = 'i'))
 		value = ft_printf_nb(va_arg(parametres, int), verification);
 	else if (str == 'u')
@@ -34,7 +33,11 @@ int	ft_print_value(va_list parametres, char str, int *verification)
 	else if (str == '%')
 		value = ft_printf_c('%', verification);
 	else
-		return (-1);
+	{
+		*verification = - 1;
+	}
+	if (value < 0)
+		*verification = -1;
 	return (value);
 }
 
@@ -43,6 +46,7 @@ int	ft_printf(const char *str, ...)
 	va_list parametres;
 	int		i;
 	int		value;
+	int		verification;
 
 	i = 0;
 	value = 0;
@@ -52,8 +56,13 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			i++;
+			value += ft_print_value(parametres,str[i], &verification);
 		}
+		else
+			value  += ft_printf_c(str[i], &verification);
 	}
-	
-
+	va_end(parametres);
+	if (verification == -1)
+		return (-1);
+	return (value);
 }
